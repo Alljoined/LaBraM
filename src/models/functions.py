f6aa59fd679b5ab9a694ded887ea778aed22eea0
bruAristimunha <a.bruno@aluno.ufabc.edu.br>
2024-02-17 21:48:14 +0100

    moving to modules and functions

---
+++ src/models/functions.py
@@ -0,0 +1,20 @@
+from timm.models.layers import trunc_normal_ as __call_trunc_normal_
+
+
+# WTF?? why define a function that returns a dictionary?
+def _cfg(url="", **kwargs):
+    return {
+        "url": url,
+        "num_classes": 1000,
+        "input_size": (3, 224, 224),
+        "pool_size": None,
+        "crop_pct": 0.9,
+        "interpolation": "bicubic",
+        "mean": (0.5, 0.5, 0.5),
+        "std": (0.5, 0.5, 0.5),
+        **kwargs,
+    }
+
+
+def trunc_normal_(tensor, mean=0.0, std=1.0):
+    __call_trunc_normal_(tensor, mean=mean, std=std, a=-std, b=std)
