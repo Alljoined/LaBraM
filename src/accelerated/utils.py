cadd0d8b8c6ddc5e3d3e2789a719329f548d6c0d
bruAristimunha <a.bruno@aluno.ufabc.edu.br>
2024-02-17 21:07:48 +0100

    black, pre-commit, moving

---
+++ src/accelerated/utils.py
@@ -0,0 +1,15 @@
+from contextlib import contextmanager
+from accelerate import init_empty_weights
+
+
+@contextmanager
+def cond_iew(condition):
+    """
+    Context that calls init_empty_weights if some condition is met (i.e. condition can be that a pretrained model is present)
+    init_empty_weights speeds up large model instantiation when a pretrained model is going to be loaded
+    """
+    if condition:
+        with init_empty_weights():
+            yield
+    else:
+        yield
