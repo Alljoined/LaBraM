0d1d3f30392839dc1f3dff9a779025da0f945e6b
bruAristimunha <a.bruno@aluno.ufabc.edu.br>
2024-03-02 01:10:05 +0100

    Changing name and removing "pre-trained" options with timm

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -2,7 +2,7 @@ from .functions import _cfg
 from .modules import (
     MLP,
     Attention,
-    Block,
+    WindowsAttentionBlock,
     DropPath,
     PatchEmbed,
     RelativePositionBias,
@@ -18,16 +18,4 @@ from .labram_pretrain import (
 from .vqnsp import (
     vqnsp_encoder_base_decoder_3x200x12,
     vqnsp_encoder_large_decoder_3x200x24,
-)
-
-from .labram_pretrain import (
-    labram_base_patch200_1600_8k_vocab,
-    labram_huge_patch200_1600_8k_vocab,
-    labram_large_patch200_1600_8k_vocab,
-)
-
-from .labram_finetune import (
-    labram_base_patch200_200,
-    labram_huge_patch200_200,
-    labram_large_patch200_200,
 )
